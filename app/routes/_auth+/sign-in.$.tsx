885e3430fd34d021f34f7dc05bde2cfd27860f2c
Junseong Park <junseongpark.dev@gmail.com>
2024-07-27 22:53:54 +0900

    feat: add auth routes

---
+++ app/routes/_auth+/sign-in.$.tsx
@@ -0,0 +1,10 @@
+import { SignIn } from '@clerk/remix'
+
+export default function SignInPage() {
+	return (
+		<div className='min-h-screen flex flex-col justify-center items-center gap-4'>
+			<h1>Sign In route</h1>
+			<SignIn />
+		</div>
+	)
+}	
---
+++ app/routes/_auth+/sign-up.$.tsx
@@ -0,0 +1,10 @@
+import { SignUp } from '@clerk/remix'
+
+export default function SignUpPage() {
+	return (
+		<div className='min-h-screen flex flex-col justify-center items-center gap-4'>
+			<h1>Sign Up route</h1>
+			<SignUp />
+		</div>
+	)
+}
